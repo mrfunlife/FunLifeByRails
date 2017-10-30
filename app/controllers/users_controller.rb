b62751c3c53fcf03e1b6d8648578fd951b053fb8
mrfunlife <dgfunlife@gmail.com>
2017-10-30 16:46:54 +0700

    Xu ly giao dien

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,45 +22,21 @@ class UsersController < ApplicationController
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
-    @user = User.new(user_params)   # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
-        :password_confirmation)
+                                 :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
 
-  root 'home#index'
-  get '/users/new',  to: 'users#new'
+
+
+  root 'sessions#new'
+
+  get 'sessions/new'
+
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   get '/home', to: 'home#home'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
